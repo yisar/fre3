5038d3e9a183285a93f6eeb29a597bfabfdf9fdf
yisar <1533540012@qq.com>
2023-04-30 21:15:15 +0800

    线性parser

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 pub mod lexer;
+pub mod parser;
 
 fn main() {
     // println!("Hello, world!");
     let str = "function App(){
         return <div>hello world</div>
     }";
-    let mut lexer = lexer::Lexer::new(str);
-    lexer.lexer();
-    println!("{:#?}", lexer.tokens)
+    let mut p = parser::Parser::new(str);
+    let root = p.parse_all();
+    // println!("{:#?}", lexer.tokens)
+    // let mut out = old_parser::parse(str.to_string());
+    println!("{:#?}", root)
+
+
 }
---
+++ src/parser.rs
@@ -0,0 +1,101 @@
+use crate::lexer::{Lexer, Token};
+use std::collections::HashMap;
+
+#[derive(PartialEq, Debug)]
+enum Actions {
+    EmptyState,
+    ReadingOpenTag,
+    ReadingCloseTag,
+}
+
+#[derive(PartialEq, Debug)]
+pub struct Node {
+    pub tag: String,
+    pub children: Vec<Node>,
+    pub props: HashMap<String, Option<String>>,
+}
+
+impl Node {
+    fn new(tag: String) -> Node {
+        Node {
+            tag,
+            children: Vec::new(),
+            props: HashMap::new(),
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct Parser {
+    pub lexer: Lexer,
+}
+
+impl Parser {
+    pub fn new(code: &str) -> Parser {
+        Parser {
+            lexer: Lexer::new(code),
+        }
+    }
+
+    pub fn parse_all(&mut self) -> Result<Node, ()> {
+        self.lexer.tokenize_all();
+        let (childs, idx) = self.parse_schild(self.lexer.tokens.clone());
+        let mut node = Node::new("root".to_string());
+        for child in childs {
+            node.children.push(child);
+        }
+
+        return Ok(node);
+    }
+
+    pub fn parse_schild(&mut self, mut slice_tokens: Vec<Token>) -> (Vec<Node>, usize) {
+        let mut child_nodes: Vec<Token> = vec![];
+        let mut idx = 0;
+        let mut state: Vec<Actions> = vec![];
+        let mut parsed: Vec<Node> = vec![];
+
+        loop {
+            if let Some(last_state) = state.last().clone() {
+                if let Some(token) = slice_tokens.get_mut(idx) {
+                    match token {
+                        Token::OpenTag(s) => {
+                            state.push(Actions::ReadingOpenTag);
+                            idx += 1;
+                        }
+
+                        Token::CloseTag(s) => {
+                            if last_state == &Actions::ReadingOpenTag {
+                                let last_idx_parsed = parsed.len() - 1;
+                                idx += 1;
+                                let tokens = slice_tokens[idx..].to_vec();
+                                println!("{:#?}", tokens);
+                                let (childs, idx) = self.parse_schild(tokens);
+                                for child in childs {
+                                    parsed[last_idx_parsed].children.push(child)
+                                }
+                            }
+                            state.push(Actions::ReadingCloseTag);
+                            idx += 1;
+                        }
+                        Token::Text(t) => {
+                            idx += 1;
+                            let n = Node::new(t.to_string());
+                            parsed.push(n);
+                        }
+
+                        _ => {
+                            idx += 1;
+                            child_nodes.push(token.clone());
+                        }
+                    }
+                } else {
+                    let node = Node::new("\n".to_string());
+                    println!("{:#?}", 111);
+                    break (parsed, idx);
+                }
+            } else {
+                state.push(Actions::EmptyState)
+            }
+        }
+    }
+}
