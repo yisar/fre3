597332dbefe633e9530c44ce47d7da96892048fd
yisar <1533540012@qq.com>
2023-05-02 15:24:24 +0800

    日常提交

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,16 @@ pub mod parser;
 fn main() {
     // println!("Hello, world!");
     let str = "function App(){
-        const count = f.signal(0)
-        return <button onclick={()=>count(count()+1)}>{count()}</button>
+        const count = f.signal([1,2,3])
+        return <div onclick={()=>count([])}>{count()
+}</div>
     }
 document.querySelector('.app').appendChild(<App/>)";
     let mut p = parser::Parser::new(str);
     let root = p.parse_all().unwrap();
     println!("{:#?}", p.lexer.tokens);
     // println!("{:#?}", root);
-    // let mut g = generator::Generator::new(root.clone());
-    // let out = g.generate();
-    // println!("{:#?}", out)
+    let mut g = generator::Generator::new(root.clone());
+    let out = g.generate();
+    println!("{:#?}", out)
 }
